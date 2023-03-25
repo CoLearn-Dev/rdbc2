84c2030e86553fdd89a879865d77502ba68f9949
nociza <azicon@berkeley.edu>
2023-03-24 18:00:20 -0700

    fix tests

--- tests/mysql_test.rs
+++ tests/mysql_test.rs
@@ -2,18 +2,18 @@ use rdbc2::dbc;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-fn _get_mysql_connection_url() -> &'static str {
+fn _get_mysql_connection_url() -> String {
     if std::env::var("MYSQL_DATABASE_URL").is_ok() {
-        std::env::var("MYSQL_DATABASE_URL").unwrap().as_str()
+        std::env::var("MYSQL_DATABASE_URL").unwrap()
     } else {
-        "mysql://localhost:3306/?user=nociza&password=password"
+        "mysql://localhost:3306/?user=nociza&password=password".to_owned()
     }
 }
 
 #[tokio::test]
 async fn test_mysql_connection() -> Result<(), Error> {
     let url = _get_mysql_connection_url();
-    let mut database = dbc::Database::new(url)?;
+    let mut database = dbc::Database::new(url.as_str())?;
     let query = "SELECT 1";
     let result = database.execute_query(query)?;
     assert_eq!(result.rows.len(), 1);
@@ -24,7 +24,7 @@ async fn test_mysql_connection() -> Result<(), Error> {
 #[tokio::test]
 async fn test_mysql_connection_with_params() -> Result<(), Error> {
     let url = _get_mysql_connection_url();
-    let mut database = dbc::Database::new(url)?;
+    let mut database = dbc::Database::new(url.as_str())?;
     let query = "SELECT ? + ?";
     let result = database.execute_query_with_params(query, &["1", "2"])?;
     assert_eq!(result.rows.len(), 1);
@@ -35,7 +35,7 @@ async fn test_mysql_connection_with_params() -> Result<(), Error> {
 #[tokio::test]
 async fn test_mysql_connection_with_params_and_serialize() -> Result<(), Error> {
     let url = _get_mysql_connection_url();
-    let mut database = dbc::Database::new(url)?;
+    let mut database = dbc::Database::new(url.as_str())?;
     let query = "SELECT ? + ?";
     let result = database.execute_query_with_params_and_serialize(query, &["1", "2"])?;
     assert_eq!(result, r#"{"rows":[{"values":[{"Bytes":[50]}],"columns":[{"name":"1 + 1","column_type":"LONGLONG"}]}]}"#);
