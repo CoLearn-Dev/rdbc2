41a4a317caba6aef4cc0259fb8361f267768953a
nociza <azicon@berkeley.edu>
2023-03-24 18:30:21 -0700

    sqlite tests

--- tests/sqlite_test.rs
+++ tests/sqlite_test.rs
@@ -2,17 +2,17 @@ use rdbc2::dbc;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-fn _get_mysql_connection_url() -> String {
-    if std::env::var("MYSQL_DATABASE_URL").is_ok() {
-        std::env::var("MYSQL_DATABASE_URL").unwrap()
+fn _get_sqlite_connection_url() -> String {
+    if std::env::var("SQLITE_DATABASE_URL").is_ok() {
+        std::env::var("SQLITE_DATABASE_URL").unwrap()
     } else {
-        "mysql://localhost:3306/?user=nociza&password=password".to_owned()
+        "sqlite://:memory:".to_owned()
     }
 }
 
 #[tokio::test]
-async fn test_mysql_simple_query() -> Result<(), Error> {
-    let url = _get_mysql_connection_url();
+async fn test_sqlite_simple_query() -> Result<(), Error> {
+    let url = _get_sqlite_connection_url();
     let mut database = dbc::Database::new(url.as_str())?;
     let query = "SELECT 1";
     let result = database.execute_query(query)?;
@@ -22,8 +22,8 @@ async fn test_mysql_simple_query() -> Result<(), Error> {
 }
 
 #[tokio::test]
-async fn test_mysql_query_with_params() -> Result<(), Error> {
-    let url = _get_mysql_connection_url();
+async fn test_sqlite_query_with_params() -> Result<(), Error> {
+    let url = _get_sqlite_connection_url();
     let mut database = dbc::Database::new(url.as_str())?;
     let query = "SELECT ? + ?";
     let result = database.execute_query_with_params(query, &["1", "2"])?;
@@ -33,8 +33,8 @@ async fn test_mysql_query_with_params() -> Result<(), Error> {
 }
 
 #[tokio::test]
-async fn test_mysql_query_with_params_and_serialize() -> Result<(), Error> {
-    let url = _get_mysql_connection_url();
+async fn test_sqlite_query_with_params_and_serialize() -> Result<(), Error> {
+    let url = _get_sqlite_connection_url();
     let mut database = dbc::Database::new(url.as_str())?;
     let query = "SELECT ? + ?";
     let result = database.execute_query_with_params_and_serialize(query, &["1", "2"])?;
